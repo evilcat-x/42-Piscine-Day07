cc389d3077f09d73c1de3f7279e2e1ce1ccf52de
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-27 16:17:45 -0700

    fix ex02

--- ex02/ft_ultimate_range.c
+++ ex02/ft_ultimate_range.c
@@ -6,7 +6,7 @@
 /*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/26 18:16:05 by seli              #+#    #+#             */
-/*   Updated: 2018/09/27 16:06:54 by seli             ###   ########.fr       */
+/*   Updated: 2018/09/27 16:17:38 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@ int		ft_ultimate_range(int **range, int min, int max)
 
 	if (min >= max)
 	{
-		*range = 0;
+		*range = (void *)0;
 		return (0);
 	}
 	*range = (int *)malloc(sizeof(int) * (((long)max - (long)min)));
 	if (!*range)
 		return (0);
 	i = 0;
-	while (min + i <= max)
+	while (min + i < max)
 	{
-		*range[i] = min + i;
+		range[0][i] = min + i;
 		i++;
 	}
-	return (i + 1);
+	return (i);
 }
